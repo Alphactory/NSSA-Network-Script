dc2fd6dc1fb04ba3feaa12d0e8fde0561ab9459c
Alphactory <aedan.e.taylor@gmail.com>
2020-09-11 13:33:48 -0400

    unbugfixes

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 import subprocess
 import os
-subprocess.run("pip install --only-binary :all: -r requirements.txt" if os.name == "nt" else "pip3 install --only-binary :all: -r requirements.txt", shell=True)
+subprocess.run("pip install -r requirements.txt" if os.name == "nt" else "pip3 install -r requirements.txt", shell=True)
